b0dc33bdf314d7f54567e20518f345db4d713ebd
Orl-GSD <egsdicipulo@addu.edu.ph>
2024-12-02 08:49:18 +0800

    Update mat quantity

---
+++ src/app/components/Modals/MatAndSubForm/EditableQty.tsx
@@ -0,0 +1,35 @@
+// This should be in your client-side component
+'use client';
+
+import React, { useState } from 'react';
+import { revalidatePath } from 'next/cache';
+import { updateQty } from '@/actionsSupabase/Update';
+
+const EditableQtyCell = ({ mat, projID, taskId }) => {
+  const [newQty, setNewQty] = useState(mat.qty);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault(); // Prevent default form submission behavior
+      if (newQty !== mat.qty) {
+        console.log(`Updating material ${mat} with qty: ${mat.qty}`);
+        updateQty(mat.id, newQty, taskId, projID);
+      }
+    }
+  };
+
+  return (
+    <form action={async (e) => {}}>
+      <input
+        name="qty"
+        type="number"
+        value={newQty}
+        className="text-center w-16 bg-transparent border border-gray-300 rounded"
+        onChange={(e) => setNewQty(Number(e.target.value))}
+        onKeyDown={handleKeyDown}
+      />
+    </form>
+  );
+};
+
+export default EditableQtyCell;
